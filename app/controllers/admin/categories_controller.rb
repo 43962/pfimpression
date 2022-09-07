5096d4ac8d265f6577df4540f374f51e602bbfcb
EC2 Default User <ec2-user@ip-172-31-39-67.ap-northeast-1.compute.internal>
2022-09-07 14:49:19 +0000

    レビュー詳細編集

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,15 +1,39 @@
 class Admin::CategoriesController < ApplicationController
   
   def index
+    @categoty = Category.new
+    @categories = Category.all
   end  
   
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "カテゴリーを追加しました"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render :index
+    end  
   end
   
   def edit
+    @category = Category.find(params[:id])
   end  
     
   def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+       flash[:notice] = "ジャンルを変更しました"
+       redirect_to admin_categories_path
+    else
+       render "edit"
+    end
+  end  
+  
+  private
+  
+  def category_params
+    params.permit(:name)
   end  
     
 end
